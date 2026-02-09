c5d8fef1f263c92c3eb71404d1b7c49486a0a338
Arne Schüler <arne@schueler.dev>
2026-02-09 12:35:49 +0100

    Fix width class for separator in LanguageButton component

--- src/components/ui/buttons/LanguageButton.jsx
+++ src/components/ui/buttons/LanguageButton.jsx
@@ -2,7 +2,7 @@ export default function LanguageButton() {
   return (
     <div className="flex items-center">
       <button className="p-3 text-cyan-500">EN</button>
-      <div className="w-0.25 h-[70%] bg-neutral-600"></div>
+      <div className="w-px h-[70%] bg-neutral-600"></div>
       <button className="p-3 text-neutral-600">DE</button>
     </div>
   );
