b79e4f534b2c831bfa71bb8a2acefab40d04057a
Arne Schüler <arne@schueler.dev>
2026-02-12 15:48:27 +0100

    added styling variables to button

--- src/components/ui/buttons/Button.jsx
+++ src/components/ui/buttons/Button.jsx
@@ -1,10 +1,11 @@
-const buttonStyle = "size-fit rounded-full text-lg font-bold border p-6";
-
-const base = "w-fit p-4 rounded-full ";
+const base = "cursor-pointer w-fit p-4 rounded-full ";
+const hover = "hover:text-cyan-500 hover:border-cyan-500";
+const active =
+  "active:bg-cyan-500/5 active:text-cyan-400 active:border-cyan-400";
 
 const variants = {
   // Button with fill
-  primary: `${base} border border-slate-500`,
+  primary: `${base} ${hover} ${active} border border-slate-500  `,
   // Button with outline
   secondary: `${base} text-cyan-500`,
   // Button as text
