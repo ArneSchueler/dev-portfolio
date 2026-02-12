1ed0f0090acfbae83d2927d6308771a22e094702
Arne Schüler <arne@schueler.dev>
2026-02-12 14:36:44 +0100

    changed icons from regular to light

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,60 +2,48 @@ import LanguageButton from "./ui/buttons/LanguageButton";
 import { scrollToSection } from "../utils/helpers";
 
 const homeIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="24px"
-    viewBox="0 -960 960 960"
-    width="24px"
-    fill="currentColor"
-  >
-    <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+    <path
+      fill="currentColor"
+      d="m217.9 110.1l-80-80a14 14 0 0 0-19.8 0l-80 80A13.92 13.92 0 0 0 34 120v96a6 6 0 0 0 6 6h64a6 6 0 0 0 6-6v-58h36v58a6 6 0 0 0 6 6h64a6 6 0 0 0 6-6v-96a13.92 13.92 0 0 0-4.1-9.9M210 210h-52v-58a6 6 0 0 0-6-6h-48a6 6 0 0 0-6 6v58H46v-90a2 2 0 0 1 .58-1.42l80-80a2 2 0 0 1 2.84 0l80 80A2 2 0 0 1 210 120Z"
+    />
   </svg>
 );
 
 const projectIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="24px"
-    viewBox="0 -960 960 960"
-    width="24px"
-    fill="currentColor"
-  >
-    <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Zm0-80h640v-400H447l-80-80H160v480Zm0 0v-480 480Z" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+    <path
+      fill="currentColor"
+      d="M216 74h-86l-28.27-21.2a14 14 0 0 0-8.4-2.8H40a14 14 0 0 0-14 14v136a14 14 0 0 0 14 14h176.89A13.12 13.12 0 0 0 230 200.89V88a14 14 0 0 0-14-14m2 126.89a1.11 1.11 0 0 1-1.11 1.11H40a2 2 0 0 1-2-2V64a2 2 0 0 1 2-2h53.33a2 2 0 0 1 1.2.4l29.87 22.4A6 6 0 0 0 128 86h88a2 2 0 0 1 2 2Z"
+    />
   </svg>
 );
 
 const aboutIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="24px"
-    viewBox="0 -960 960 960"
-    width="24px"
-    fill="currentColor"
-  >
-    <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm146.5-204.5Q340-521 340-580t40.5-99.5Q421-720 480-720t99.5 40.5Q620-639 620-580t-40.5 99.5Q539-440 480-440t-99.5-40.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm100-95.5q47-15.5 86-44.5-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160q53 0 100-15.5ZM523-537q17-17 17-43t-17-43q-17-17-43-17t-43 17q-17 17-17 43t17 43q17 17 43 17t43-17Zm-43-43Zm0 360Z" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+    <path
+      fill="currentColor"
+      d="M229.19 213c-15.81-27.32-40.63-46.49-69.47-54.62a70 70 0 1 0-63.44 0C67.44 166.5 42.62 185.67 26.81 213a6 6 0 1 0 10.38 6c19.21-33.19 53.15-53 90.81-53s71.6 19.81 90.81 53a6 6 0 1 0 10.38-6M70 96a58 58 0 1 1 58 58a58.07 58.07 0 0 1-58-58"
+    />
   </svg>
 );
 
 const contactIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="24px"
-    viewBox="0 -960 960 960"
-    width="24px"
-    fill="currentColor"
-  >
-    <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+    <path
+      fill="currentColor"
+      d="M224 50H32a6 6 0 0 0-6 6v136a14 14 0 0 0 14 14h176a14 14 0 0 0 14-14V56a6 6 0 0 0-6-6m-15.42 12L128 135.86L47.42 62ZM216 194H40a2 2 0 0 1-2-2V69.64l86 78.78a6 6 0 0 0 8.1 0L218 69.64V192a2 2 0 0 1-2 2"
+    />
   </svg>
 );
 
-const activeItem = "pb-1 text-cyan-500 border-b-2";
+const activeItem = " w-9 pb-1 text-cyan-500 border-b-2";
 const itemBase =
-  "text-neutral-600 pb-1  border-b-2 border-transparent hover:text-cyan-500 ";
+  "w-9 text-neutral-600 pb-1  border-b-2 border-transparent hover:text-cyan-500 ";
 
 function Logo() {
   return (
-    <a href="/" className="font-tech md:writing-vertical md:rotate-180">
+    <a href="/" className="font-tech md:rotate-180">
       schueler.dev
     </a>
   );
@@ -72,7 +60,7 @@ function Sidebar({ activeSection }) {
   return (
     <>
       {/* Desktop Sidebar */}
-      <aside className="hidden bg-white box-border sm:flex flex-col fixed h-full justify-between items-center px-6 pt-20 pb-8 shadow-xl">
+      <aside className="hidden bg-white box-border sm:flex flex-col fixed h-full justify-between items-center px-6 pt-20 pb-8 ">
         <Logo />
         <div className="flex flex-col gap-8">
           {NAV_ITEMS.map(({ id, label, Icon }) => {
@@ -95,14 +83,14 @@ function Sidebar({ activeSection }) {
       </aside>
 
       {/* Mobile Sidebar Header */}
-      <aside className="flex justify-between sm:hidden fixed w-full bg-white items-center p-6 shadow-xl z-10">
+      <aside className="flex justify-between sm:hidden fixed w-full bg-white items-center py-2 px-4 z-10">
         <Logo />
         <LanguageButton />
       </aside>
 
       {/* Mobile Sidebar Navigation */}
-      <aside className="flex justify-center sm:hidden fixed bottom-0 bg-white w-full p-6 shadow-xl z-10">
-        <div className="flex gap-8">
+      <aside className="flex justify-center sm:hidden fixed bottom-0 bg-white w-full p-6 z-10">
+        <div className="flex w-full justify-between px-6">
           {NAV_ITEMS.map(({ id, href, label, Icon }) => {
             const isActive = activeSection === id;
 
