c582e8b03e220623d713cda9e96b0d464bf2272f
Arne Schüler <arne@schueler.dev>
2026-02-13 14:45:24 +0100

    moved navItems to own config file

---
+++ src/data/nav.config.js
@@ -0,0 +1,6 @@
+export const NAV_ITEMS = [
+  { id: "hero", label: "Hero", icon: "ph:house" },
+  { id: "projects", label: "Projects", icon: "ph:folder" },
+  { id: "about", label: "About", icon: "ph:user" },
+  { id: "contact", label: "Contact", icon: "ph:envelope" },
+];
