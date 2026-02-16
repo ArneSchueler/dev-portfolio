ab798c218bf6409fa570f8d9a3fb3ca3a7e2c19b
Arne Schüler <arne@schueler.dev>
2026-02-16 09:25:16 +0100

    refactored icons link tag

--- src/components/ui/buttons/Button.jsx
+++ src/components/ui/buttons/Button.jsx
@@ -6,14 +6,12 @@ const primaryBtn = "px-4 py-2 rounded-full border  ";
 
 export const ctaBtn = {
   title: "View Projects",
-  route: "/projects",
   icon: "ph:arrow-right-light",
   className: `${primaryBtn}`,
   isExternal: false,
 };
 export const enBtn = {
   title: "EN",
-  route: "",
   icon: "",
   className: "",
   isExternal: false,
@@ -29,7 +27,6 @@ export const deBtn = {
 export const projectDetailsBtn = {
   title: "View Projects",
   icon: "ph:arrow-right-light",
-  route: "/:detailProject",
   className: `text-cyan-500`,
   isExternal: false,
 };
@@ -74,42 +71,10 @@ export default function Button({
     );
   } else {
     return (
-      <a href={route} className={className}>
+      <a href={route} className={className} target="_blank" rel="noreferrer">
         {title}
-        <Icon icon={icon} />
+        <Icon icon={icon} className="size-7" />
       </a>
     );
   }
 }
-
-// const base = "cursor-pointer w-fit p-4 rounded-full ";
-// const hover = "hover:text-cyan-500 hover:border-cyan-500";
-// const active =
-//   "active:bg-cyan-500/5 active:text-cyan-400 active:border-cyan-400";
-
-// const variants = {
-//   // Button with fill
-//   primary: `${base} ${hover} ${active} border border-slate-500  `,
-//   // Button with outline
-//   secondary: `${base} text-cyan-500`,
-//   // Button as text
-//   tertiary: "",
-// };
-
-// export default function Button({
-//   onClick,
-//   children,
-//   ariaLabel,
-//   variant = "primary",
-//   link,
-// }) {
-//   const className = variants[variant];
-
-//   return (
-//     <button className={className} aria-label={ariaLabel} onClick={onClick}>
-//       <a href={link} target="_blank">
-//         {children}
-//       </a>
-//     </button>
-//   );
-// }
