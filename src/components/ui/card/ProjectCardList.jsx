061f779589f78875150f06928f6c9567218ab3f4
Arne Schüler <arne@schueler.dev>
2026-02-16 09:26:28 +0100

    refactored icons and external links

--- src/components/ui/card/ProjectCardList.jsx
+++ src/components/ui/card/ProjectCardList.jsx
@@ -3,22 +3,27 @@ import { projectDetailsBtn, githubBtn, redirectBtn } from "../buttons/Button";
 
 export default function ProjectCardList({ items }) {
   return (
-    <div className="flex flex-col xl:flex-row lg:col-span-3 gap-14">
-      {items.map(({ id, title, description }) => {
+    <div className="flex flex-col gap-14 lg:col-span-3 xl:flex-row">
+      {items.map(({ slug, overview }) => {
+        const { title, description, links = [] } = overview;
+
+        const github = links.find((link) => link.type === "github")?.href;
+        const external = links.find((link) => link.type === "external")?.href;
+
         return (
           <article
-            key={id}
-            className="flex flex-col gap-6 border-b-2 p-4 hover:bg-slate-200/10 hover:rounded-t-2xl hover:scale-102 hover:border-b-cyan-500"
+            key={slug}
+            className="flex flex-col gap-6 border-b-2 p-4 hover:scale-102 hover:rounded-t-2xl hover:border-b-cyan-500 hover:bg-slate-200/10"
           >
             <div className="flex flex-col gap-4">
               <h3>{title}</h3>
               <p>{description}</p>
             </div>
-            <div className="flex justify-between   pb-6">
-              <Button {...projectDetailsBtn} route={`/project/${id}`} />
-              <div>
-                <Button {...githubBtn} />
-                <Button {...redirectBtn} />
+            <div className="flex justify-between pb-6">
+              <Button {...projectDetailsBtn} route={`/project/${slug}`} />
+              <div className="flex gap-4">
+                {github && <Button {...githubBtn} route={github} />}
+                <Button {...redirectBtn} route={external} />
               </div>
             </div>
           </article>
