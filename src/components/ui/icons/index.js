bfc67034b5354a10bb49babe5a87474dfe352744
Arne Schüler <arne@schueler.dev>
2026-02-16 09:26:53 +0100

    refactored ICONS

---
+++ src/components/ui/icons/index.js
@@ -0,0 +1,12 @@
+export const ICONS = {
+  home: "ph:house-light",
+  projects: "ph:folder-simple-light",
+  about: "ph:user-light",
+  contact: "ph:envelope-simple-light",
+  arrowRight: "ph:arrow-right-light",
+  github: "ph:github-logo-light",
+  behance: "ph:behance-logo-light",
+  external: "ph:arrow-square-out-light",
+  light: "ph:sun-dim-light",
+  dark: "ph:moon-light",
+};
